0b99ab0ac55ed0811d7a510ae61945234e556f6b
dev-JVelandia <juanvelandiaguerrero@gmail.com>
2026-09-22 06:57:19 -0500

    messi

---
+++ operaciones/calculadora.py
@@ -0,0 +1,12 @@
+
+#Se debe implenetar la logica de potenciacion
+def potenciacion_asterscos(base,exponente):
+    print(base**exponente)
+
+potenciacion_asterscos(5,2)
+
+#Se debe implementar la logica de la raiz cuadrada
+def raiz_cuadrada_asteriscos(base):
+    print(base**0.5)
+
+raiz_cuadrada_asteriscos(121)
